f887cc4e8329217fa42377d918f6c0deca872f18
Pete Hainlen <phainlen@gmail.com>
2026-08-28 19:22:35 -0500

    Update lineup.ts

--- src/worker/routes/lineup.ts
+++ src/worker/routes/lineup.ts
@@ -3,14 +3,9 @@ import { KvStore } from "../lib/kv";
 import { sleeper } from "../lib/sleeper";
 import { planLineup } from "../cron/planLineup";
 import type { Env } from "../index";
+import { sleeper, resolveFantasyWeek } from "../lib/sleeper";
 
 export const lineupRoute = new Hono<{ Bindings: Env }>();
-
-function resolveFantasyWeek(nflState: { week: number; season_type: string }): number {
-  // Sleeper's `week` field tracks preseason weeks too during "pre" season_type.
-  // Fantasy lineups don't apply until the regular season starts.
-  return nflState.season_type === "regular" ? nflState.week : 1;
-}
 
 lineupRoute.get("/", async (c) => {
   const kv = new KvStore(c.env.DUCK_KV);
